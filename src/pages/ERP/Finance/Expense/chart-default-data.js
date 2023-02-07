dd618ceb046f96981fbb91717c1676040da37aaf
nstig <124514833+tomapull@users.noreply.github.com>
2023-02-08 01:24:22 +0900

    fixed:table modified and expense page added

---
+++ src/pages/ERP/Finance/Expense/chart-default-data.js
@@ -0,0 +1,34 @@
+const barchart = {
+  series: [{
+    name: "Series",
+    data: [
+      481,
+      349,
+      231,
+      881,
+      327,
+      304,
+      576,
+    ]
+  }],
+  options: {
+    xaxis: {
+      type: 'category',
+      categories: [
+        '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+      ],
+    },
+    chart: {
+      toolbar: {
+        show: false,
+        },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    colors: '#82567A',
+  },
+
+} 
+
+export default barchart;

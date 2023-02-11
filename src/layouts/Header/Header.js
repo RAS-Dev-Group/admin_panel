db7c7a507523efd4220823afaf8e32b7c0123288
PassionMan1130 <123367712+ElvisJin@users.noreply.github.com>
2023-02-12 01:10:49 +0900

    Merge

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import HeaderDropDown from './HeaderDropdown';
+import HeaderDropDown from "./HeaderDropdown";
 import Search from "./Search";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";

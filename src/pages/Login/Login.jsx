1984809f8cc25feb3292fbddbc7589187824b19b
PassionMan1130 <123367712+ElvisJin@users.noreply.github.com>
2023-02-14 07:48:51 +0900

    added api, router, and modified login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,107 +1,104 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.scss";
-import axios from "axios";
+import API from '../../utils/api';
 
-class FluidInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      focused: false,
-      value: "",
-    };
+function FluidInput({ type, label, id, defValue = '', onChange }) {
+  let [focused, setFocused] = useState(false);
+  let [value, setValue] = useState(defValue);
+
+  function focusField() {
+    setFocused(!focused);
   }
 
-  focusField() {
-    const { focused } = this.state;
-    this.setState({
-      focused: !focused,
-    });
+  function handleChange(event) {
+    setValue(event.target.value);
+    onChange(value);
   }
 
-  handleChange(event) {
-    const { target } = event;
-    const { value } = target;
-    this.setState({
-      value: value,
-    });
-  }
-
-  render() {
-    const { type, label, style, id } = this.props;
-    const { focused, value } = this.state;
-
-    let inputClass = "fluid-input";
-    if (focused) {
-      inputClass += " fluid-input--focus";
-    } else if (value !== "") {
-      inputClass += " fluid-input--open";
-    }
-
-    return (
-      <div className={inputClass} style={style}>
-        <div className="fluid-input-holder">
-          <input
-            className="fluid-input-input"
-            type={type}
-            id={id}
-            onFocus={this.focusField.bind(this)}
-            onBlur={this.focusField.bind(this)}
-            onChange={this.handleChange.bind(this)}
-            autoComplete="off"
-          />
-          <label className={"fluid-input-label" + (this.state.value ? ' invisible' : '')} forhtml={id}>
-            {label}
-          </label>
-        </div>
-      </div>
-    );
-  }
-}
-
-function Button({ buttonClass, onClick, buttonText }) {
   return (
-    <div
-      className={`button ${buttonClass}`}
-      onClick={onClick}
-    >
-      {buttonText}
+    <div className={"fluid-input !my-4" + (focused ? ' fluid-input--focus' : (value !== '' ? ' fluid-input--open' : ''))}>
+      <div className="fluid-input-holder">
+        <input
+          className="fluid-input-input"
+          type={type}
+          id={id}
+          onFocus={focusField}
+          onBlur={focusField}
+          onChange={handleChange}
+          value={value}
+          autoComplete="off"
+        />
+        <label className={"fluid-input-label" + (value ? ' invisible' : '')} forhtml={id}>
+          {label}
+        </label>
+      </div>
     </div>
   );
 }
 
-export default function LoginContainer() {
-  const style = {
-    margin: "15px 0",
-  };
+function Button({ buttonClass, onClick, buttonText }) {
+  return (
+    <button
+      className={`button ${buttonClass}`}
+      onClick={onClick}
+    >
+      {buttonText}
+    </button>
+  );
+}
 
+export default function LoginContainer() {
   const navigate = useNavigate();
   const [appState, setAppState] = useState({
     loading: false,
-    repos: null
+    name: 'bipin',
+    password: 'Bipin@123'
   });
 
+  function onNameChange(name) {
+    setAppState({ ...appState, name });
+  }
+
+  function onPasswordChange(password) {
+    setAppState({ ...appState, password });
+  }
+
   function handleSubmit(e) {
-    // navigate('/erp/project');
-    // return false;
+    const loginData = new FormData();
+    loginData.append('username', appState.name);
+    loginData.append('password', appState.password);
 
-    const logindata = new FormData();
-    logindata.append('username', 'bipin');
-    logindata.append('password', 'Bipin@123');
+    console.log(appState);
 
-    setAppState({ loading: true });
+    if (!appState.name) {
+      alert('Please input name');
+      return;
+    }
+
+    if (!appState.password) {
+      alert('Please input password');
+      return;
+    }
+
+    setAppState({ ...appState, loading: true });
     e.preventDefault();
-    axios({
-            // Endpoint to send files
-            url: "https://furniture-dusky.vercel.app/token",
-            method: "POST",
-            data: logindata,
-    })
+    API.post("/token", loginData)
       .then((data) => {
-        setAppState({ loading: false });
-        console.log(data);
+        setAppState({ ...appState, loading: false });
+        console.log('response', data);
         // store data.access_token ; we will use this for ...
-        // navigate('/erp/project');
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        else {
+          alert('You are logged in');
+          // save access_token to somewhere (for global use)
+          console.log('token', data.access_token);
+          navigate('erp/project');
+          return;
+        }
       })
       .catch((err) => {
         console.log(err.messag);
@@ -112,13 +109,8 @@ export default function LoginContainer() {
     <div className="flex h-screen login-page">
       <form className="login-container">
         <div className="title">Login</div>
-        <FluidInput type="text" label="name" id="name" style={style} />
-        <FluidInput
-          type="password"
-          label="password"
-          id="password"
-          style={style}
-        />
+        <FluidInput type="text" label="name" id="name" defValue={appState.name} onChange={onNameChange} />
+        <FluidInput type="password" label="password" id="password" defValue={appState.password} onChange={onPasswordChange} />
         <Button
           buttonText="log in"
           onClick={handleSubmit}

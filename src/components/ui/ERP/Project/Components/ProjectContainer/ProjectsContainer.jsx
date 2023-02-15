b5596fb0c0212e3974168e5eddebddf84043ac47
tomapull <leon.kaminski.0105@gmail.com>
2023-02-15 20:50:24 +0900

    solve the conflict

--- src/components/ui/ERP/Project/Components/ProjectContainer/ProjectsContainer.jsx
+++ src/components/ui/ERP/Project/Components/ProjectContainer/ProjectsContainer.jsx
@@ -18,7 +18,6 @@ import ModalTask from './ModalProjectEdit/ModalTask';
 //  import ModalDetailProejct Component
 import ModalProjectDetail from './ModalProjectDetail/ModalProjectDetail';
 
-<<<<<<< HEAD
 import axios from "axios";
 
 const samplemembers = [
@@ -67,8 +66,6 @@ const samplemembers = [
     created_at: new Date(),
   },
 ];
-=======
->>>>>>> 0bba1fc0ca7bf8ce5ab4bd65410abdd7ae4edcc3
 
 const sampleproject = {
   _id: {
@@ -105,7 +102,6 @@ const sampleproject = {
 axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiaXBpbiIsImV4cCI6MTY3NjQwMjQwM30.xftGwJvjm47IV4m62pIHTFWWbASpUJOub0eX_IF-vrY';
 
 export default function ProjectsContainer(props) {
-<<<<<<< HEAD
 
   const [projects, setProject] = useState([{}]);
   const [showProjectModal, setShowProjectModal] = useState(false);
@@ -135,104 +131,6 @@ export default function ProjectsContainer(props) {
       });
   }, []);
 
-=======
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null
-  });
-  const [projects, setProjects] = useState([]);
-  const [cookie, setCookie, removeCookie] = useCookies(['token']);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // load projects on mount
-    console.log('token', cookie.token);
-    getProjects(cookie.token)
-      .then(res => {
-        // show projects
-        if (res.data.error) {
-          console.log(res.data.error);
-          if (res.data.error === 'Signature has expired.') {
-            removeCookie('token');
-            console.log('navigate login');
-            navigate('/login');
-          }
-        }
-        else {
-          // show projects
-          setProjects(res.data.data);
-        }
-      })
-      .catch(err => {
-        // error occured
-      });
-  }, []);
-
-  let key = 0;
-
-  const description =
-    "Lorem ipsum dolor sit amet consectetur. In nunc nunc donec bibendum risus. Amet amet est viverra condimentum sed praesent. Velit quis lectus pulvinar elementum nulla. Et rhoncus id habitant augue neque. Elementum tempor amet bibendum consectetur sem mattis est elementum sed. Odio velit egestas elit nulla nunc consequat diam morbi nec. \
-  Nec arcu sagittis orci fames gravida sed etiam. Feugiat maecenas pellentesque massa tempor. Fermentum placerat dictum vivamus et accumsan consequat mauris lorem feugiat. Nisi lorem pellentesque proin lacus convallis at. Luctus massa vitae diam volutpat ipsum eget.";
-  const members = [
-    {
-      id: 1,
-      username: "member1",
-      full_name: "Member 1",
-      role: "string",
-      email: "member1@gmail.com",
-      password: "password1",
-      contact: 0,
-      address: "Address 1",
-      created_at: new Date(),
-    },
-    {
-      id: 2,
-      username: "member2",
-      full_name: "Member 2",
-      role: "string",
-      email: "member2@gmail.com",
-      password: "password2",
-      contact: 0,
-      address: "Address 2",
-      created_at: new Date(),
-    },
-    {
-      id: 3,
-      username: "member3",
-      full_name: "Member 3",
-      role: "string",
-      email: "member1@gmail.com",
-      password: "password3",
-      contact: 0,
-      address: "Address 3",
-      created_at: new Date(),
-    },
-    {
-      id: 4,
-      username: "member4",
-      full_name: "Member 4",
-      role: "string",
-      email: "member1@gmail.com",
-      password: "password4",
-      contact: 0,
-      address: "Address 4",
-      created_at: new Date(),
-    },
-  ];
-  const tasks = [
-    { name: "Task 1", description: 'This is the first Task' },
-    { name: "Task 2", description: 'This is the seconde Task' },
-    { name: "Task 3", description: 'This is the three Task' },
-  ];
-  const [showProjectModal, setShowProjectModal] = useState(false);
-  const [showDetailModal, setShowDetailModal] = useState(false);
-  const [showTaskModal, setShowTaskModal] = useState(false);
-
-  const [currentTasks, setCurrentTask] = useState([{}]);
-
-  const [currentMembers, setCurrentMembers] = useState([{}]);
-
->>>>>>> 0bba1fc0ca7bf8ce5ab4bd65410abdd7ae4edcc3
   const handleAddTask = (task) => {
     let tasklist = currentTasks;
 
@@ -302,21 +200,9 @@ export default function ProjectsContainer(props) {
             </button>
           </div>
           <div className="flex flex-wrap justify-evenly items">
-<<<<<<< HEAD
             {projects.map((project, index) => (
               <ProjectItem key={index} data={project} showdetail={handleProjectDetailModalOpen} />                
             ))}              
-=======
-            {
-              projects.map(project => (
-                <ProjectItem
-                  key={project._id.$oid}
-                  project={project}
-                  showdetail={handleProjectDetailModalOpen}
-                />
-              ))
-            }
->>>>>>> 0bba1fc0ca7bf8ce5ab4bd65410abdd7ae4edcc3
           </div>
           <ModalProject
             currenttasks={currentTasks}
